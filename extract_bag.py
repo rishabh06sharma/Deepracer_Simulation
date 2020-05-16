0aa60ae661f33aff7682ebb15daa34363bce4f53
Rishabh Sharma <60935966+rs278@users.noreply.github.com>
2020-05-16 19:14:44 -0400

    Update and rename altertest.py to extract_bag.py

--- extract_bag.py
+++ extract_bag.py
@@ -1,10 +1,8 @@
 import rosbag
-#from PIL import Image
 import numpy as np
 import time
 from cv_bridge import CvBridge, CvBridgeError
 import os.path
-#bag1 = rosbag.Bag('2020-04-15-01-19-42.bag')
 bag1=rosbag.Bag('hardclock.bag')
 train=np.array([])
 lim=[]
@@ -17,9 +15,6 @@ for a,b,c in bag1:
     if(a=='/camera/zed/rgb/image_rect_color'):
 
         frame = bridge.imgmsg_to_cv2(b, "bgr8")
-#        img = Image.fromarray(frame,'RGB')
-        #newsize = (160,120)
-#        im1 = img.resize(newsize)
         lim.append(frame)
     elif(a=='/vesc/low_level/ackermann_cmd_mux/output'):
         i+=1
@@ -29,31 +24,19 @@ for a,b,c in bag1:
             img = lim[0]
         except:
             continue
-        #print(img.shape)
         try:
             train = np.vstack((train, np.array([img])))
             test = np.vstack((test, np.array([steer])))
         except:
             train=np.array([img])
             test = np.array([steer])
-        #print(train.shape)
-        # print(train)
-        #test.append(steer)
-        #flush list of images
         lim=[]
         if(i==10000):
             break
 
 
-# train=np.array(train)
-# test=np.array(test)
-#
 print(img.shape)
 print(train.shape)
 print(test.shape)
-
-
-
 bag1.close()
-
 np.savez_compressed('/tmp/hardclockv1',a=train,b=test)
